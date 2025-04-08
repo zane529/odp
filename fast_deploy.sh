33c70770003de6dbaa716821d3da28d7f2614289
Zane <18938064829@163.com>
2025-04-08 21:39:24 +0800

    update 00_Demo_Features.ipynb

--- fast_deploy.sh
+++ fast_deploy.sh
@@ -10,19 +10,15 @@ pip install ipyleaflet
 pip install cogeo_mosaic
 pip install localtileserver
 pip install rasterio
-pip install matplotlib folium
-pip install geopandas pycrs osmnx
+pip install matplotlib 
+pip install folium
+pip install geopandas 
+pip install pycrs 
+pip install osmnx
 pip install leafmap
 pip install segment-geospatial
+pip install fiona
 pip install --find-links=https://girder.github.io/large_image_wheels --no-cache GDAL
-
-# Update from GitHub
-echo "Optional update of leafmap ..."
-pip install git+https://github.com/opengeos/leafmap
-
-# Install from GitHub
-echo "Optional update of Geospatial AIML Developer Enviorment Setup ..."
-pip install git+https://github.com/opengeos/segment-geospatial
 
 # Conda Packages 
 echo "Java Geo-Display Enviorment Setup ..."
@@ -31,7 +27,3 @@ conda install -y jupyter_contrib_nbextensions -c conda-forge
 # jupyter nbextension enable widgetsnbextension --py --sys-prefix
 # jupyter labextension install js
 jupyter labextension list
-
-# Create Kernel for Notebook
-# ipython kernel install --name "python-geo" --user
-
